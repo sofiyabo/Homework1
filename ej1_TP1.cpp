9d4368d7bb694e52355d99c24bf2feaac717d706
sofiyabo <161760235+sofiyabo@users.noreply.github.com>
2025-03-14 11:03:58 -0300

    Add files via upload

---
+++ ej1_TP1.cpp
@@ -0,0 +1,13 @@
+/* a. Crear una función que dado un valor entero “n”, positivo y mayor a uno, devuelva una matriz cuadrada con valores como 
+en los siguientes ejemplos para n=2 y n=3:*/
+
+//container std::array
+#include <iostream>
+#include <vector>
+
+
+int matriz_cuadrada(int n){
+    std::vector<std::vector<int>> matriz_cuad(n, std::vector<int>(n));
+    
+
+}
