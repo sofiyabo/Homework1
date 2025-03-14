4eefee995f15c3029d15ea9e7c5cdacbfdcffe4a
Sofi Yabo <syabo@udesa.edu.ar>
2025-03-14 19:21:03 -0300

    comienzo ejercicio 2

--- Ejercicio2/ej2.cpp
+++ Ejercicio2/ej2.cpp
@@ -4,4 +4,22 @@ void logMessage(String mensaje, Integer/Otro NivelSeveridad)
 Donde NivelSeveridad corresponderá con unas de las leyendas previamente mencionadas.
 
 */
+#include <iostream>
+#include <fstream>
+using namespace std;
 
+
+void logMessage(string mensaje, string nivel){
+    ofstream archivo("registro.txt", ios::app);
+    if(archivo.is_open()){
+        archivo<< "[" << nivel << "] <" << mensaje << ">\n";
+        archivo.close();
+    }
+    else{
+        cerr<< "Error abriendo archivo\n";
+    }
+}
+
+int main(){
+    logMessage("faltan datos", "INFO");
+}
