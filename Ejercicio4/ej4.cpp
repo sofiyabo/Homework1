147d3cd7be353c8d1957fad34e5a877594254a2c
Sofi Yabo <syabo@udesa.edu.ar>
2025-03-21 13:28:20 -0300

    funcion en tiempo de compilacion

--- Ejercicio4/ej4.cpp
+++ Ejercicio4/ej4.cpp
@@ -17,13 +17,27 @@ bool textos_iguales(const char* texto1, const char* texto2){
     return textos_iguales(texto1 + 1, texto2 +1); //El +1 agarra el proximo caracter
 }
 
+constexpr bool textos_iguales_comp(const char* texto1, const char* texto2){
+    if(*texto1 == '\0' && *texto2 == '\0') return true;
+    if(*texto1 != *texto2) return false;
+    return textos_iguales_comp(texto1 + 1, texto2 +1);
+}
+
 int main(){
     
     auto startTime = chrono::high_resolution_clock::now();
-    textos_iguales("porque yo no quiero trabajar, no quiero ir a estudiar, no me quiero casar", "porque yo no quiero trabajar, no quiero ir a estudiar, no me quiero casar");
+    textos_iguales("porque yo no quiero trabajar, no quiero ir a estudiar, no me quiero casar. Quiero tocar la guitarra todo el dia, y que la gente se enamore de mi voz.", "porque yo no quiero trabajar, no quiero ir a estudiar, no me quiero casar. Quiero tocar la guitarra todo el dia, y que la gente se enamore de mi voz.");
     auto endTime = chrono::high_resolution_clock::now();
     auto elapsedTime = chrono::duration_cast<chrono::nanoseconds>( endTime - startTime);
     cout << "A miProcesoAMedir le tomó:" << elapsedTime.count() << "nanosegundos" << endl;
     
 
+    auto startTime1 = chrono::high_resolution_clock::now();
+    textos_iguales_comp("porque yo no quiero trabajar, no quiero ir a estudiar, no me quiero casar. Quiero tocar la guitarra todo el dia, y que la gente se enamore de mi voz.", "porque yo no quiero trabajar, no quiero ir a estudiar, no me quiero casar. Quiero tocar la guitarra todo el dia, y que la gente se enamore de mi voz.");
+    auto endTime1 = chrono::high_resolution_clock::now();
+    auto elapsedTime1 = chrono::duration_cast<chrono::nanoseconds>( endTime1 - startTime1);
+    cout << "A miProcesoAMedir le tomó:" << elapsedTime1.count() << "nanosegundos" << endl;
+    
 }
+
+//La diferencia 
